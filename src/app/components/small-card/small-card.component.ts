ce58451adb83b77edacd17749cfc95406147998d
FelipinDaMassa <feliperibeiroaraujo3@hotmail.com>
2023-01-04 10:31:38 -0300

    deixando dinamico o big card e o small card

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./small-card.component.css']
 })
 export class SmallCardComponent implements OnInit {
+  photoCover:string = 'https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg'
+  cardTitle:string = 'NOVA SÉRIE ANUNCIADA'
 
   constructor() { }
 
