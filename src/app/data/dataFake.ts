5a75a38a463598e50fcb7d564f9bff2729d31452
FelipinDaMassa <feliperibeiroaraujo3@hotmail.com>
2023-01-05 10:06:30 -0300

    adicionando rotas

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,16 @@
+export const dataFake = [
+{
+  "id":"1",
+  "title":"saiu um novo filme do Iron Man",
+  "description":"Marvel anuncia um novo filme",
+  "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/AED07A4BEFCAD750C7C2B03E648A1C3D06C2AEFCFE20D6A19F2F6ECE6584F521/scale?width=1200&aspectRatio=1.78&format=jpeg"
+
+},
+{
+  "id":"2",
+  "title":"Nova serie anunciada",
+  "description":"lorem ipsum dois dois dois",
+  "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+}
+
+];
